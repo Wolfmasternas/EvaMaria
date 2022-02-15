babb1a038bfff8326348a93b9b4f5fb5c612d84f
Wolfmasternas <98648610+Wolfmasternas@users.noreply.github.com>
2022-02-15 12:01:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -40,7 +40,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/NarutoCinema_Bot)</code>
+<code>[Button Text](buttonurl:http://t.me/Sadiex_Bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
